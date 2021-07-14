efaad7699df60782029d4eb58581e950856f1040
ANAS-ALGO-TECH <anas001ismail@gmail.com>
2021-07-14 16:39:11 +0500

    finishing Sign In handleSubmit method

--- src/App.js
+++ src/App.js
@@ -32,10 +32,11 @@ class App extends React.Component{
                             id : snapShot.id,
                             ...snapShot.data()
                         }
-                    },
-                    () => {
-                        console.log(this.state);
-                        }
+                    }
+                    // ,
+                    // () => {
+                    //     console.log(this.state);
+                    //     }
                     );
                 });
             }else{
